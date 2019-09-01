e92893e074e946aae12f3815ec388bfe57b752e1
Timo Röder <roeder.t17@gmail.com>
2019-09-01 19:47:19 +0200

    add metadata check

--- R/mapstat_metadata.R
+++ R/mapstat_metadata.R
@@ -33,7 +33,33 @@ get_multiple_metadata <- function(input, type = "directory", ...) {
   tmp_list <- purrr::map(input_files, get_single_metadata, ...)
 
   mapstat_metadata <- dplyr::bind_rows(tmp_list)
-  # Print quick summary
+
+  check_metadata(mapstat_metadata)
 
   return(mapstat_metadata)
+}
+
+#' Check mapstat metadata
+#'
+#' @param meta_df Metadata data frame that should be checked. It is expected that
+#'   you imported this one with \code{\link{get_multiple_metadata}}.
+#'
+#' @return Nothing is returned. Only prints a summary the console
+#' @export
+check_metadata <- function(meta_df) {
+  cat("--------- Mapstat metadata summary ---------\n")
+
+  cat(paste0("No. of mapstat files:\t", nrow(meta_df), "\n"))
+  cat(paste0("Date of mapping:\t", ifelse(length(unique(meta_df$date)) == 1,
+                                          as.character(unique(meta_df$date)),
+                                          paste(range(meta_df$date), collapse = " to ")), "\n"))
+  cat(paste0("Mapping method(s):\t", paste(unique(meta_df$method), collapse = ", "), "\n"))
+  cat(paste0("Method version(s):\t", paste(unique(meta_df$method_version), collapse = ", "), "\n"))
+  cat(paste0("Database version(s):\t", paste(unique(meta_df$db_version), collapse = ", "), "\n"))
+
+  column_conflicts <- apply(meta_df[, 2:4], 2, function(x) length(unique(x)) > 1)
+  if(any(column_conflicts))
+    message("\nCareful! The following fields have more than one entry:\n",
+            paste(names(which(column_conflicts)), collapse = ", "), "\n\n",
+            "Consider remapping so that all samples are mapped in the same manner.")
 }
